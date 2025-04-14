0c516e613e6df637925b0bed4dc154556397671a
Chengyi-Hua <96745479+Chengyi-Hua@users.noreply.github.com>
2025-04-14 22:30:54 +0200

    Update _getting_the_data.py

--- _getting_the_data.py
+++ _getting_the_data.py
@@ -1,17 +1,17 @@
 import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-# OPTIONAL: Automatically move kaggle.json to the right place
-kaggle_json_path = 'path_to_your_downloaded/kaggle.json'  # replace with actual path
-os.makedirs(os.path.expanduser('~/.kaggle'), exist_ok=True)
-os.system(f'cp {kaggle_json_path} ~/.kaggle/kaggle.json')
-os.chmod(os.path.expanduser('~/.kaggle/kaggle.json'), 0o600)
+# In case you need it, as never used Kaggle API:
+# Automatically move kaggle.json to the right place (you will need to create it on your Kaggle Account)
+# kaggle_json_path = 'path_to_your_downloaded/kaggle.json'  # replace with actual path (the downloaded kaggle.json API file)
+# os.makedirs(os.path.expanduser('~/.kaggle'), exist_ok=True)
+# os.system(f'cp {kaggle_json_path} ~/.kaggle/kaggle.json')
+# os.chmod(os.path.expanduser('~/.kaggle/kaggle.json'), 0o600)
 
-# Initialize API
+#API
 api = KaggleApi()
 api.authenticate()
 
-# Download dataset
+#Download
 api.dataset_download_files('jessemostipak/hotel-booking-demand', path='hotel_booking_data', unzip=True)
 
-print("✅ Dataset downloaded and extracted into 'hotel_booking_data' folder.")
